7604711d604af215646fe46f1e4e10326e8c18f0
79jonathan <jonathanescalona@gmail.com>
2022-12-12 12:35:00 -0400

    envio a proyecto_1 de historias locas

---
+++ proyecto1_historias_locas.py
@@ -0,0 +1,20 @@
+# concatenar cademas de cararcteres.
+# suponga,os que queremps crear una cadena que diga:
+# aprende a programar con___________________
+
+# organizacion = "freecodecamp"
+# print("\b")
+# print("aprende a programar con " + organizacion)
+# print("aprende a programar con {}".format(organizacion))
+# print(f"aprende a programar con {organizacion}")
+
+# mad libs (historias locas)
+
+adj = input("adjetivo: ")
+verbo1 = input("verbo: ")
+verbo2 = input("verbo: ")
+sustantivo_plural = input("sustantivo (plural): ")
+
+madlib = f"!programar es tan {adj} siempre me emociona porque me encanta {verbo1} problemas. !aprende a {verbo2} con freecodecamp y alcanza tus {sustantivo_plural} "
+
+print(madlib)
